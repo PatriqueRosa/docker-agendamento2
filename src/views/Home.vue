eddfc977bf743dc859edd37182ec9efe0f5b7a1f
Patrique Eduardo da Rosa <le_pick07@hotmail.com>
2025-02-21 20:10:43 -0300

    Update Home.vue
    add button para '/create'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,17 @@
   <div class="container text-center mt-5">
     <h1>Home</h1>
     <p>Bem-vindo à página inicial!</p>
+    <button @click="goToCreate" class="btn btn-secondary mb-3">Ir para agendamento</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home'
-}
+},
+  goToCreate() {
+    this.$router.push('/create');
+  }
 </script>
 
 <style scoped>

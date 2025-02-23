272b45a7168d7d92f1c768d2de7611da992957b4
Patrique Rosa <le_pick07@hotmail.com>
2025-02-23 19:43:41 -0300

    atualização api.ts para Render

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://docker-agendamento.onrender.com'
 });
 
 // Adicionar um interceptor para incluir o token JWT no cabeçalho das requisições
